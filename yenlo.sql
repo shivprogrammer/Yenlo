dd77e139614224a255632463b8a435206b576e1d
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-28 17:30:24 -0800

    organizing and inserting values into the necessary tables

--- yenlo.sql
+++ yenlo.sql
@@ -1,5 +1,14 @@
 CREATE TABLE yenlo (productID INTEGER PRIMARY KEY, name TEXT)
 
-CREATE TABLE stock (amount INTEGER, location TEXT)
+-- Each location will have a different amount of stock available for the product
+CREATE TABLE stock (location TEXT, amount INTEGER)
 
+-- The products
 INSERT INTO yenlo (productID, name)
+VALUES (1, 'Basketball');
+VALUES (2, 'Microphone');
+VALUES (3, 'Piano');
+VALUES (4, 'Book');
+VALUES (5, 'Laptop');
+
+-- The locations
