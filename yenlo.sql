3254e23c96e929ce07a397997f455f45dd94e7ab
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-28 18:00:24 -0800

    fixed up the database foundational information

--- yenlo.sql
+++ yenlo.sql
@@ -1,14 +1,32 @@
-CREATE TABLE yenlo (productID INTEGER PRIMARY KEY, name TEXT)
+-- To establish the database
+DROP DATABASE IF EXISTS yenlo;
+CREATE DATABASE yenlo;
+USE yenlo;
+
+--------------------------------------------------------
+-- Creation of the products table
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (productID INTEGER PRIMARY KEY, name TEXT);
 
 -- The products
-INSERT INTO yenlo (productID, name)
+INSERT INTO products (productID, name)
 VALUES (1, 'Basketball');
+
+INSERT INTO products (productID, name)
 VALUES (2, 'Microphone');
+
+INSERT INTO products (productID, name)
 VALUES (3, 'Piano');
+
+INSERT INTO products (productID, name)
 VALUES (4, 'Book');
+
+INSERT INTO products (productID, name)
 VALUES (5, 'Laptop');
 --------------------------------------------------------
 -- Each location will have a different amount of stock available for the product
+
+DROP TABLE IF EXISTS stock;
 CREATE TABLE stock (productID INTEGER FOREIGN KEY, location TEXT, amount INTEGER)
 
 -- The locations:
@@ -16,32 +34,40 @@ CREATE TABLE stock (productID INTEGER FOREIGN KEY, location TEXT, amount INTEGER
 -- Boston
 -- Austin
 
+-- Seattle stock information
 INSERT INTO stock (productID, location, amount)
 VALUES (1, 'Seattle', 10);
+INSERT INTO stock (productID, location, amount)
 VALUES (2, 'Seattle', 10);
+INSERT INTO stock (productID, location, amount)
 VALUES (3, 'Seattle', 10);
+INSERT INTO stock (productID, location, amount)
 VALUES (4, 'Seattle', 10);
+INSERT INTO stock (productID, location, amount)
 VALUES (5, 'Seattle', 10);
 
+-- Boston stock information
+INSERT INTO stock (productID, location, amount)
 VALUES (1, 'Boston', 20);
+INSERT INTO stock (productID, location, amount)
 VALUES (2, 'Boston', 20);
+INSERT INTO stock (productID, location, amount)
 VALUES (3, 'Boston', 20);
+INSERT INTO stock (productID, location, amount)
 VALUES (4, 'Boston', 20);
+INSERT INTO stock (productID, location, amount)
 VALUES (5, 'Boston', 20);
 
+-- Austin stock information
+INSERT INTO stock (productID, location, amount)
 VALUES (1, 'Austin', 30);
+INSERT INTO stock (productID, location, amount)
 VALUES (2, 'Austin', 30);
+INSERT INTO stock (productID, location, amount)
 VALUES (3, 'Austin', 30);
+INSERT INTO stock (productID, location, amount)
 VALUES (4, 'Austin', 30);
+INSERT INTO stock (productID, location, amount)
 VALUES (5, 'Austin', 30);
 
 --------------------------------------------------------
-
-CREATE TABLE seattle (productID INTEGER FOREIGN KEY, basketball_amount INTEGER, microphone_amount INTEGER, piano_amount INTEGER, book_amount INTEGER, laptop_amount INTEGER);
-INSERT INTO seattle (productID, basketball_amount, microphone_amount, piano_amount, book_amount, laptop_amount )
-
-CREATE TABLE boston (productID INTEGER FOREIGN KEY, basketball_amount INTEGER, microphone_amount INTEGER, piano_amount INTEGER, book_amount INTEGER, laptop_amount INTEGER);
-INSERT INTO boston (productID, basketball_amount, microphone_amount, piano_amount, book_amount, laptop_amount )
-
-CREATE TABLE austin (productID INTEGER FOREIGN KEY, basketball_amount INTEGER, microphone_amount INTEGER, piano_amount INTEGER, book_amount INTEGER, laptop_amount INTEGER);
-INSERT INTO austin (productID, basketball_amount, microphone_amount, piano_amount, book_amount, laptop_amount )
