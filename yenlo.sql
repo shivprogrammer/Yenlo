fb6a4b3523dcba9780e4a7c013cb2b2139659246
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-28 17:44:54 -0800

    found a better solution to organize the data

--- yenlo.sql
+++ yenlo.sql
@@ -1,8 +1,5 @@
 CREATE TABLE yenlo (productID INTEGER PRIMARY KEY, name TEXT)
 
--- Each location will have a different amount of stock available for the product
-CREATE TABLE stock (productID INTEGER FOREIGN KEY, location TEXT, amount INTEGER)
-
 -- The products
 INSERT INTO yenlo (productID, name)
 VALUES (1, 'Basketball');
@@ -10,11 +7,35 @@ VALUES (2, 'Microphone');
 VALUES (3, 'Piano');
 VALUES (4, 'Book');
 VALUES (5, 'Laptop');
+--------------------------------------------------------
+-- Each location will have a different amount of stock available for the product
+CREATE TABLE stock (productID INTEGER FOREIGN KEY, location TEXT, amount INTEGER)
 
--- The locations
+-- The locations:
 -- Seattle
 -- Boston
 -- Austin
+
+INSERT INTO stock (productID, location, amount)
+VALUES (1, 'Seattle', 10);
+VALUES (2, 'Seattle', 10);
+VALUES (3, 'Seattle', 10);
+VALUES (4, 'Seattle', 10);
+VALUES (5, 'Seattle', 10);
+
+VALUES (1, 'Boston', 20);
+VALUES (2, 'Boston', 20);
+VALUES (3, 'Boston', 20);
+VALUES (4, 'Boston', 20);
+VALUES (5, 'Boston', 20);
+
+VALUES (1, 'Austin', 30);
+VALUES (2, 'Austin', 30);
+VALUES (3, 'Austin', 30);
+VALUES (4, 'Austin', 30);
+VALUES (5, 'Austin', 30);
+
+--------------------------------------------------------
 
 CREATE TABLE seattle (productID INTEGER FOREIGN KEY, basketball_amount INTEGER, microphone_amount INTEGER, piano_amount INTEGER, book_amount INTEGER, laptop_amount INTEGER);
 INSERT INTO seattle (productID, basketball_amount, microphone_amount, piano_amount, book_amount, laptop_amount )
