51eb94bc760f687d06fe8ab2aeb45d0b408e8dac
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-19 04:48:36 -0700

    added individual itemID values to each one of the yenloStock rows

--- yenlo.sql
+++ yenlo.sql
@@ -1,5 +1,4 @@
 -- POSSIBLE TODO: May need to seperate out each city into its own table, for security purposes.
--- TODO: add primary key to yenloStock
 
 -- To establish the database
 DROP DATABASE IF EXISTS yenlo;
@@ -31,60 +30,60 @@ VALUES (5, 'Super Saiyan T-Shirt', 19.99);
 -- Creating and establishing the amount of stock for various yenlo store locations
 DROP TABLE IF EXISTS yenloStock;
 
--- itemID is an unique ID that is combination of productID and an arbitrary representation of location
+-- itemID is an unique ID that is combination of productID and an arbitrary numerical representation of location
 CREATE TABLE yenloStock (itemID DECIMAL(2, 2) PRIMARY KEY, productID INTEGER, location TEXT, amount INTEGER, FOREIGN KEY (productID) REFERENCES yenloProducts(productID));
 
 -- The locations:
+-- Online
 -- Seattle
 -- Boston
 -- Austin
--- Online
-
--- Seattle stock information
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (1, 'Seattle', 10);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (2, 'Seattle', 10);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (3, 'Seattle', 10);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (4, 'Seattle', 10);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (5, 'Seattle', 10);
-
--- Boston stock information
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (1, 'Boston', 20);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (2, 'Boston', 20);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (3, 'Boston', 20);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (4, 'Boston', 20);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (5, 'Boston', 20);
-
--- Austin stock information
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (1, 'Austin', 30);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (2, 'Austin', 30);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (3, 'Austin', 30);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (4, 'Austin', 30);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (5, 'Austin', 30);
 
 -- Online stock amount information
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (1, 'Online', 40);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (2, 'Online', 40);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (3, 'Online', 40);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (4, 'Online', 40);
-INSERT INTO yenloStock (productID, location, amount)
-VALUES (5, 'Online', 40);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (0.1, 1, 'Online', 40);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (0.2, 2, 'Online', 40);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (0.3, 3, 'Online', 40);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (0.4, 4, 'Online', 40);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (0.5, 5, 'Online', 40);
+
+-- Seattle stock information
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.1, 1, 'Seattle', 10);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.2, 2, 'Seattle', 10);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.3, 3, 'Seattle', 10);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.4, 4, 'Seattle', 10);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.5, 5, 'Seattle', 10);
+
+-- Boston stock information
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (2.1, 1, 'Boston', 20);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (2.2, 2, 'Boston', 20);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (2.3, 3, 'Boston', 20);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (2.4, 4, 'Boston', 20);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (2.5, 5, 'Boston', 20);
+
+-- Austin stock information
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.1, 1, 'Austin', 30);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.2, 2, 'Austin', 30);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.3, 3, 'Austin', 30);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.4, 4, 'Austin', 30);
+INSERT INTO yenloStock (itemID, productID, location, amount)
+VALUES (1.5, 5, 'Austin', 30);
 --------------------------------------------------------
