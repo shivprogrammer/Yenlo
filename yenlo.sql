ff6ee13c2f7c5ab6e98f1cab3f478b672d3798a0
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-19 04:43:48 -0700

    swtiched table back and added itemID property to yenloStock

--- yenlo.sql
+++ yenlo.sql
@@ -1,4 +1,5 @@
 -- POSSIBLE TODO: May need to seperate out each city into its own table, for security purposes.
+-- TODO: add primary key to yenloStock
 
 -- To establish the database
 DROP DATABASE IF EXISTS yenlo;
@@ -6,9 +7,32 @@ CREATE DATABASE yenlo;
 USE yenlo;
 
 --------------------------------------------------------
+-- Creation of the yenloProducts table
+DROP TABLE IF EXISTS yenloProducts;
+CREATE TABLE yenloProducts (productID INTEGER PRIMARY KEY, name TEXT, price DECIMAL(5, 2));
+
+-- The products
+INSERT INTO yenloProducts (productID, name, price)
+VALUES (1, 'Calming Water Painting', 49.99);
+
+INSERT INTO yenloProducts (productID, name, price)
+VALUES (2, 'Microphone to Sing like Bieber', 399.99);
+
+INSERT INTO yenloProducts (productID, name, price)
+VALUES (3, 'Dave Chappelle Comedy Collection', 24.99);
+
+INSERT INTO yenloProducts (productID, name, price)
+VALUES (4, 'Sweet Gaming Chair', 99.99);
+
+INSERT INTO yenloProducts (productID, name, price)
+VALUES (5, 'Super Saiyan T-Shirt', 19.99);
+--------------------------------------------------------
+
 -- Creating and establishing the amount of stock for various yenlo store locations
 DROP TABLE IF EXISTS yenloStock;
-CREATE TABLE yenloStock (productID INTEGER PRIMARY KEY, location TEXT, amount INTEGER);
+
+-- itemID is an unique ID that is combination of productID and an arbitrary representation of location
+CREATE TABLE yenloStock (itemID DECIMAL(2, 2) PRIMARY KEY, productID INTEGER, location TEXT, amount INTEGER, FOREIGN KEY (productID) REFERENCES yenloProducts(productID));
 
 -- The locations:
 -- Seattle
@@ -63,25 +87,4 @@ INSERT INTO yenloStock (productID, location, amount)
 VALUES (4, 'Online', 40);
 INSERT INTO yenloStock (productID, location, amount)
 VALUES (5, 'Online', 40);
---------------------------------------------------------
-
--- Creation of the yenloProducts table
-DROP TABLE IF EXISTS yenloProducts;
-CREATE TABLE yenloProducts (productID INTEGER, name TEXT, price DECIMAL(5, 2), FOREIGN KEY (productID) REFERENCES yenloStock(productID));
-
--- The products
-INSERT INTO yenloProducts (productID, name, price)
-VALUES (1, 'Calming Water Painting', 49.99);
-
-INSERT INTO yenloProducts (productID, name, price)
-VALUES (2, 'Microphone to Sing like Bieber', 399.99);
-
-INSERT INTO yenloProducts (productID, name, price)
-VALUES (3, 'Dave Chappelle Comedy Collection', 24.99);
-
-INSERT INTO yenloProducts (productID, name, price)
-VALUES (4, 'Sweet Gaming Chair', 99.99);
-
-INSERT INTO yenloProducts (productID, name, price)
-VALUES (5, 'Super Saiyan T-Shirt', 19.99);
 --------------------------------------------------------
